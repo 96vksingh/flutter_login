6c15d35cb88bef4c7298dae5f3a0b05941979ab8
Adarsh Shukla <50777364+ideallyadarsh@users.noreply.github.com>
2020-03-31 12:30:51 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
 import 'loginpage.dart';
 
-void main(){
+void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    
     return new MaterialApp(
-      title: 'flutter',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-
-      ),
-      home: new LoginPage()
-    );
+        title: 'flutter',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new LoginPage());
   }
-
-
-}
+} //hey
